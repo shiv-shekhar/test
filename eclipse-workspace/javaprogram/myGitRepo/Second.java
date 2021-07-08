ddb103343f52259b9b344e322e26f975810e3d3b
S Shekhar <shivamshekkhar@hotmail.com>
2021-07-08 17:34:47 +0530

    added some changes

--- eclipse-workspace/javaprogram/myGitRepo/Second.java
+++ eclipse-workspace/javaprogram/myGitRepo/Second.java
@@ -3,7 +3,10 @@ import java.util.*;
 
 public class Second {
 	public static void main(String[] args) {
-		System.out.println("My HomeWork!");
+		int a = 10, b = 20;
+		int sum = a + b;
+		System.out.println("sum:" +sum);
+		//System.out.println("My HomeWork!");
 		
 	}
 
