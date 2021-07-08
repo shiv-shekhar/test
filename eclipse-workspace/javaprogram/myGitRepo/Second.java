16f3c28a28a1e6f924c143c2721c5943213916e3
Shivam Shekhar <45821075+shiv-shekhar@users.noreply.github.com>
2021-07-08 17:24:57 +0530

    Update Second.java

--- eclipse-workspace/javaprogram/myGitRepo/Second.java
+++ eclipse-workspace/javaprogram/myGitRepo/Second.java
@@ -3,10 +3,7 @@ import java.util.*;
 
 public class Second {
 	public static void main(String[] args) {
-		int a = 10, b=15;
-		int c = a + b;
-		System.out.println(c);
-		//System.out.println("My HomeWork!");
+		System.out.println("My HomeWork!");
 		
 	}
 
